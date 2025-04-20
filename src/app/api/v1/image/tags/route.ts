44693cfbef9724e2d058e8ac33fcff6af6dd0860
Braden Zingler <bradenzingler@gmail.com>
2025-04-20 08:43:14 -0500

    Basic image tags working

---
+++ src/app/api/v1/image/tags/route.ts
@@ -0,0 +1,36 @@
+import { Rekognition } from "@aws-sdk/client-rekognition";
+
+// https://docs.aws.amazon.com/AWSJavaScriptSDK/v3/latest/client/rekognition/command/DetectLabelsCommand/
+const client = new Rekognition();
+
+export async function POST(req: Request) {
+	const { image_url } = await req.json();
+
+	if (!image_url) {
+		return Response.json(
+			{ error: "'image_url' field is required" },
+			{ status: 400 }
+		);
+	}
+
+	const response = await fetch(image_url);
+	const imageArrayBuffer = await response.arrayBuffer();
+
+	const labels = await client.detectLabels({
+		Image: {
+			Bytes: new Uint8Array(imageArrayBuffer),
+		},
+		MaxLabels: 10,
+		MinConfidence: 70,
+	});
+
+	const tags = labels.Labels?.map((label) => label.Name?.toLowerCase()) || [];
+	if (!tags.length) {
+		return Response.json(
+			{ error: "No tags could be found" },
+			{ status: 404 }
+		);
+	}
+
+	return Response.json({ tags });
+}

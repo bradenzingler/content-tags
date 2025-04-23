1b72f1b03cc44bdfdb533ae0487534b8f47c575c
Braden Zingler <bradenzingler@gmail.com>
2025-04-23 11:05:47 -0500

    make dir if not exists, add error response function to lamdba

--- backend/lambda/src/main.go
+++ backend/lambda/src/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"context"
+	"fmt"
 	// "os"
 	"encoding/json"
 
@@ -10,18 +11,35 @@ import (
 	"github.com/aws/aws-lambda-go/lambda"
 )
 
-// var client openai.Client = *openai.NewClient(os.Getenv("OPENAI_API_KEY")) 
+// var client openai.Client = *openai.NewClient(os.Getenv("OPENAI_API_KEY"))
 
 type ResponseBody struct {
 	Tags []string `json:"tags"`
 }
 
+type ErrorResponseBody struct {
+	ErrorCode 			string `json:"error_code"`
+	DocumentationURL 	string `json:"documentation_url"`
+	ErrorDescription 	string `json:"error_description"`
+}
+
+type RequestBody struct {
+	ImageUrl string `json:"image_url"`
+}
+
 func handler(ctx context.Context, request events.APIGatewayProxyRequest) (events.APIGatewayProxyResponse, error) {
+	
+	var requestBody RequestBody
+	err := json.Unmarshal([]byte(request.Body), &requestBody)
+
+	if err != nil {
+		fmt.Println(err)
+		return errorResponse("invalid_request", "The request body could not be parsed. Check your input body for typos.", 400)
+	}
 
 	response := ResponseBody{
 		Tags: []string{"123", "456"},
 	}
-
 
 	responseJSON, err := json.Marshal(response)
 	if err != nil {
@@ -34,6 +52,21 @@ func handler(ctx context.Context, request events.APIGatewayProxyRequest) (events
 	return events.APIGatewayProxyResponse{
 		StatusCode: 200,
 		Body:  		string(responseJSON),
+	}, nil
+}
+
+func errorResponse(code string, description string, status int) (events.APIGatewayProxyResponse, error) {
+	errResp := ErrorResponseBody{
+		ErrorCode: code,
+		DocumentationURL: "https://inferly.org/docs/errors/" + code,
+		ErrorDescription: description,
+	}
+
+	jsonBody, _ := json.Marshal(errResp);
+
+	return events.APIGatewayProxyResponse{
+		StatusCode: status,
+		Body: 		string(jsonBody),
 	}, nil
 }
 

02c01e23c76d5d9ef45f0114964be0b0924721b8
Braden Zingler <bradenzingler@gmail.com>
2025-04-25 09:42:51 -0500

    Add more utils tests

--- __tests__/setup/setup.ts
+++ __tests__/setup/setup.ts
@@ -1,31 +1,6 @@
 import { beforeEach } from "node:test";
 import { vi } from "vitest";
 
-vi.mock("@/lib/unkey", () => ({
-	unkey: {
-		keys: {
-			verify: vi.fn(),
-		},
-	},
-	API_ID: "mock-api-id",
-}));
-
-vi.mock("openai", () => {
-	const createMock = vi.fn();
-	return {
-        OpenAI: vi.fn().mockImplementation(() => ({
-            responses: {
-                create:  createMock,
-            },
-        })),
-        __esModule: true,
-        createMock,
-	};
-});
-
-vi.mock("@/lib/s3", () => ({
-    putImageInS3AndGetPresignedUrl: vi.fn(),
-}));
 
 beforeEach(() => {
 	vi.resetAllMocks();
